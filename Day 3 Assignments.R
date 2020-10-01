c82b60cd534148d083f8785699a2e7c969ba7da0
chepuru313 <72225247+chepuru313@users.noreply.github.com>
2020-10-02 00:58:49 +0530

    Add files via upload

---
+++ Day 3 Assignments.R
@@ -0,0 +1,49 @@
+n <-c (1,2.4,5)
+O <-c (3,4.4,5.5)
+n+O
+n-O
+n/O
+n%%O
+x<-c(8)
+y<-(20)
+x<-c(9)
+y<-c(30)
+8^2
+x+y  
+P <-c(8,9,10)
+Q <-c(10,12,15)
+P>Q
+P<Q
+P==Q
+P>=Q
+Q>=P
+P!=Q
+matrix1 <- matrix(c(3, 9, -1, 4, 2, 6), nrow = 2)
+print(matrix1)
+matrix2 <- matrix (c(5, 2, 0, 9, 3, 4), nrow = 2)
+print(matrix2)
+result <- matrix1 * matrix2
+print(result)
+DF<-data.frame(ID=c(1,2,3,4),name=c('d','k','l','s'),marks=c(9,9.5,8,8.5))
+DF
+DF2<-data.frame(ID=c(1,2,3),name=c('ch','LS','DC'),time=c(1,2,3))
+DF2
+ a<-matrix(c(1,2,3,4),nrow = 2,byrow = 1)
+a
+x<-matrix(c(1:5),nrow = 5,ncol = 5,byrow = f,dimnames = list(c('r1','r2','r3')(c('c1','c2','c3'))))
+x
+arr<-array(c(1:6),dim = c(2,4,3))
+arr
+sahithi<-list(ID=c(1,2,3,4),name=c('D','K','L','s'),Marks=c(9,9.5,8,8.5))
+sahithi
+DF<-data.frame(ID=c(1,2,3,4),name=c('d','k','l','s'),marks=c(9,9.5,8,8.5))
+DF
+DF<-data.frame(ID=cD(261,262,263,264),name=c('c','k','r','t'),marks=c(10,10.5,11,11.5))
+DF
+a<-c(1,2,3,4,'a')
+a
+DF<-data.frame(x=c(1,2,3),y=c('a','b','c'))
+DF
+DF[1]
+DF[1,a]
+DF[1,]
